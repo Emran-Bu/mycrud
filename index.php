31c9a842f1f76f2004bd1768205bef45cc915ac9
Freelancer-Emran <mdemranhasan809@gmail.com>
2022-08-30 12:15:13 +0600

    mycrud index file

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- style css -->
+    <link rel="stylesheet" href="css/style.css">
+    <title>My_CRUD</title>
+</head>
+<body>
+    <!-- start main div -->
+
+    <div class="mainDiv">
+         <!-- start header div -->
+        <div class="header">
+            <h1>My CRUD</h1>
+        </div>
+         <!-- end header div -->
+
+          <!--start menu div -->
+        <div class="menuDiv">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Add</a></li>
+                <li><a href="#">Update</a></li>
+                <li><a href="#">Delete</a></li>
+            </ul>
+        </div>
+         <!-- end main div -->
+
+          <!-- star table div -->
+          <div class="tableDiv">
+                <h3>All Students Records</h3>
+                <table class="table">
+                    <th>
+                        <td>ID</td>
+                        <td>Name</td>
+                        <td>Class</td>
+                        <td>Address</td>
+                        <td>Phone</td>
+                        <td>Action</td>
+                    </th>
+                </table>
+          </div>
+           <!-- end table div -->
+
+         <!-- end main div -->
+    </div>
+</body>
+</html>
