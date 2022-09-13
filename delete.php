78352c1da2ec31dfd5a5998236609519bb5b016d
Freelancer-Emran <mdemranhasan809@gmail.com>
2022-09-13 10:10:42 +0600

    delete image

--- delete.php
+++ delete.php
@@ -8,6 +8,17 @@
 
         $conn = mysqli_connect("localhost", "root", "", "mycrud") or die('Query Unsuccessfully');
 
+        $sql1 = "SELECT * FROM student WHERE sid = $delete_id";
+
+        $result = mysqli_query($conn, $sql1) or die('Query Unsuccessfully');
+
+        $row = mysqli_fetch_assoc($result);
+
+        $delete_image = $row['simage'];
+
+        unlink('img/'.$delete_image);
+
+
         $sql = "DELETE FROM student WHERE sid = {$delete_id}";
     
         $result = mysqli_query($conn, $sql) or die('Query Failed');
